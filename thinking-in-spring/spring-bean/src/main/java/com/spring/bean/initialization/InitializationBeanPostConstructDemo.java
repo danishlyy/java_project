9144f6f60f3394411444d1eb2e9fc875356439d0
yongyongli <liyongyong@fofund.com.cn>
2022-05-22 20:33:57 +0800

    feat:Bean的延迟初始化

--- thinking-in-spring/spring-bean/src/main/java/com/spring/bean/initialization/InitializationBeanPostConstructDemo.java
+++ thinking-in-spring/spring-bean/src/main/java/com/spring/bean/initialization/InitializationBeanPostConstructDemo.java
@@ -5,6 +5,7 @@ import com.spring.bean.factory.impl.DefaultUserFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 @Configuration // 可配置的Bean
 public class InitializationBeanPostConstructDemo {
@@ -13,12 +14,29 @@ public class InitializationBeanPostConstructDemo {
         AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext();
         annotationConfigApplicationContext.register(InitializationBeanPostConstructDemo.class);
         annotationConfigApplicationContext.refresh();
+        System.out.println("spring 上下文已经启动...");
         UserFactory userFactory = annotationConfigApplicationContext.getBean(UserFactory.class);
+        System.out.println(userFactory);
         annotationConfigApplicationContext.close();
     }
 
-
+    /**
+     * @Lazy(value=true) 使用后的输出结果  按需加载 触发依赖查找
+     * spring 上下文已经启动...
+     * PostConstruct UserFactory 初始化中
+     * 使用InitializingBean#afterPropertiesSet 初始化userFactory
+     * 自定义初始化方法 initUserFactory() : UserFactory 初始化中...
+     * com.spring.bean.factory.impl.DefaultUserFactory@6328d34a
+     * @Lazy(value=false) 使用后的输出结果
+     * PostConstruct UserFactory 初始化中
+     * 使用InitializingBean#afterPropertiesSet 初始化userFactory
+     * 自定义初始化方法 initUserFactory() : UserFactory 初始化中...
+     * spring 上下文已经启动...
+     * com.spring.bean.factory.impl.DefaultUserFactory@7f9fcf7f
+     * @return
+     */
     @Bean(initMethod = "initUserFactory")
+    @Lazy
     public UserFactory userFactory(){
         return new DefaultUserFactory();
     }
