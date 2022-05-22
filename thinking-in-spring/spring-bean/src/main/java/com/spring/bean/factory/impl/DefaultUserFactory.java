29397cffd5103022729ce322d748213b9829380c
yongyongli <liyongyong@fofund.com.cn>
2022-05-22 20:47:25 +0800

    feat:Bean的销毁

--- thinking-in-spring/spring-bean/src/main/java/com/spring/bean/factory/impl/DefaultUserFactory.java
+++ thinking-in-spring/spring-bean/src/main/java/com/spring/bean/factory/impl/DefaultUserFactory.java
@@ -1,16 +1,23 @@
 package com.spring.bean.factory.impl;
 
 import com.spring.bean.factory.UserFactory;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
-public class DefaultUserFactory implements UserFactory , InitializingBean {
+public class DefaultUserFactory implements UserFactory , InitializingBean, DisposableBean {
 
 
     @PostConstruct
     public void init(){
         System.out.println("PostConstruct UserFactory 初始化中");
+    }
+
+    @PreDestroy
+    public void destroyMethod(){
+        System.out.println("PreDestroy UserFactory 销毁中");
     }
 
     @Override
@@ -23,5 +30,15 @@ public class DefaultUserFactory implements UserFactory , InitializingBean {
     @Override
     public void afterPropertiesSet() throws Exception {
         System.out.println("使用InitializingBean#afterPropertiesSet 初始化userFactory");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("DisposableBean#destroy 销毁中");
+    }
+
+    @Override
+    public void destroyUserFactory() {
+        System.out.println("自定义销毁方法 destroyUserFactory() : UserFactory 销毁中...");
     }
 }
--- thinking-in-spring/spring-bean/src/main/java/com/spring/bean/initialization/InitializationBeanPostConstructDemo.java
+++ thinking-in-spring/spring-bean/src/main/java/com/spring/bean/initialization/InitializationBeanPostConstructDemo.java
@@ -17,7 +17,15 @@ public class InitializationBeanPostConstructDemo {
         System.out.println("spring 上下文已经启动...");
         UserFactory userFactory = annotationConfigApplicationContext.getBean(UserFactory.class);
         System.out.println(userFactory);
+        System.out.println("spring 上下文准备关闭中...");
+//        close方法调用，触发销毁
+//        spring 上下文准备关闭中...
+//        PreDestroy UserFactory 销毁中
+//        DisposableBean#destroy 销毁中
+//        自定义销毁方法 destroyUserFactory() : UserFactory 销毁中...
+//        spring 上下文已关闭...
         annotationConfigApplicationContext.close();
+        System.out.println("spring 上下文已关闭...");
     }
 
     /**
@@ -33,9 +41,14 @@ public class InitializationBeanPostConstructDemo {
      * 自定义初始化方法 initUserFactory() : UserFactory 初始化中...
      * spring 上下文已经启动...
      * com.spring.bean.factory.impl.DefaultUserFactory@7f9fcf7f
+     *
+     * 销毁执行顺序：
+     * PreDestroy UserFactory 销毁中
+     * DisposableBean#destroy 销毁中
+     * 自定义销毁方法 destroyUserFactory() : UserFactory 销毁中...
      * @return
      */
-    @Bean(initMethod = "initUserFactory")
+    @Bean(initMethod = "initUserFactory",destroyMethod = "destroyUserFactory")
     @Lazy
     public UserFactory userFactory(){
         return new DefaultUserFactory();
